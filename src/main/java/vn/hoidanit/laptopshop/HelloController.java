3c2b12a498c42db5ceb2d0a7ab1843f1f06bea57
Tuan Kiet <Tuankie102@gmail.com>
2024-04-23 19:35:49 +0700

    update pom devtools and mapping user, admin

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,6 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World from Spring Boot! with Tuan Kiet";
+        return "Hello World from Spring Boot! with Tuan Kiet, let's study";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only for user";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only for admin";
     }
 }
