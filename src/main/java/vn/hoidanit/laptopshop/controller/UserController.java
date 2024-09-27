a4076dd783fd604d6fb7dd98a0fd0a113b0e933e
Tuan Kiet <Tuankie102@gmail.com>
2024-09-27 23:56:29 +0700

    ap dung mo hinh mvc

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,30 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+// @Controller
+// public class UserController {
+
+//     @RequestMapping("/")
+//     public String getHomePage() {
+//         return "hello from controller";
+//     }
+// }
+
+@RestController
+public class UserController {
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage() {
+        return this.userService.handleHello();
+    }
+}
