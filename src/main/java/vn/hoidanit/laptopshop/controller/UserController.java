b2bf6874198adfea214612b929885487e24cd887
Tuan Kiet <Tuankie102@gmail.com>
2024-09-28 15:11:29 +0700

    use mvc

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +16,13 @@ public class UserController {
         this.userService = userService;
     }
 
+    // MVC: view gửi rq tới controller xong ctrl yêu cầu lấy data từ model xong
+    // model trả về ctrl xong ctrl phản hổi view
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("tuankiet", "from with controller model");
         return "hello";
     }
 }
--- src/main/webapp/WEB-INF/view/hello.jsp
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -1,13 +1,22 @@
-<html lang="en">
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+        <html lang="en">
 
-<body>
-    Hello from jsp
-</body>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
 
-</html>
+        <body>
+            Hello from jsp
+            <h1>
+                ${eric}
+            </h1>
+            <h2>
+                ${tuankiet}
+            </h2>
+        </body>
+
+        </html>
